4e1691e9dac8d836a896e22ceb4bcb6a5199babf
hamzasherxdxd <hamzasher223@gmail.com>
2022-07-23 00:41:00 +0500

    Removed Unnecessay links

--- src/components/nav.js
+++ src/components/nav.js
@@ -5,99 +5,6 @@ import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 
 class NavScrollExample extends React.Component {
-    // const { data, setData } = useFetch();
-    // function handleChange(event) {
-    //   // (event) => setData({ ...data, slug: event.target.value })
-    //   data.slug = searchTerm.current.value;
-    //   setData({ ...data, slug: data.slug });
-    //   console.log(data);
-    //   console.log(data.results);
-    //     showData();
-    // }
-
-    // function showData() {
-    //   console.log(data.results);
-    //     return <Result results={data.results} />;
-    // }
-
-    // constructor(props) {
-    //     super(props)
-    //     this.state = {
-    //         val: '',
-    //         results: [],
-    //     }
-    //     this.handleChange = this.handleChange.bind(this)
-    //     // this.renderResult = this.renderResult.bind(this)
-    // }
-
-    // componentDidUpdate(prevProps, prevState) {
-    //     console.log(
-    //         'PrevState: ' + prevState.val + 'CurrentState: ' + this.state.val
-    //     )
-    //     if (prevState.val !== this.state.val) {
-    //         this.fetchData(this.state.val)
-            
-    //     }
-    //     console.log((this.state.results))
-    // }
-
-    // handleChange(e) {
-    //     // console.log('val at handlechange: ' + e.target.value)
-    //     // console.log('State value: ' + this.state.val)
-    //     this.setState({
-    //         val: e.target.value,
-    //     })
-    //     console.log(this.state.val)
-    //     // this.fetchData(this.state.val);
-    // }
-
-    // fetchData(artist_name) {
-    //     artist_name = artist_name.replace(/ /g, '_')
-    //     console.log('artist name: ' + artist_name)
-    //     // const timeoutId = setTimeout(() => {
-    //     const fetch = async () => {
-    //         try {
-    //             const res = await got.get(`/searchalbum.php?s=${artist_name}`)
-    //             this.setState({
-    //                 ...this.state.val,
-    //                 results: res.data,
-    //             })
-    //             console.log(this.state.results)
-    //             // setData({...data, results: res.data});
-    //         } catch (err) {
-    //             console.error(err)
-    //         }
-    //     }
-    //     fetch()
-
-    //     // , 1000)
-    //     // return () => clearTimeout(timeoutId)
-    // }
-
-    // handleRender = (event) => {
-    //     this.props.parentCallback(this.state.val);
-    // }
-
-    // renderResult() {
-    //     console.log(this.state.val);
-    //     return <Result query={this.state.val} />
-    // }
-
-    // let searchTerm = React.createRef();
-
-    // const handleChange = () => {
-    //   console.log(searchTerm.current.value);
-    //   return <Result query={searchTerm.current.value} />;
-    // }
-
-    // function handleChange(){
-    //   return <Result query={searchTerm.current.value} />;
-    // }
-
-    // handleChange(e) {
-    //     e.target.select();
-    // }
-
     render() {
         return (
             <div>
@@ -111,8 +18,7 @@ class NavScrollExample extends React.Component {
                                 style={{ maxHeight: '100px' }}
                                 navbarScroll
                             >
-                                <Nav.Link href="#">Home</Nav.Link>
-                                <Nav.Link href="#">About</Nav.Link>
+                                <Nav.Link>Type artist name to search for albums.</Nav.Link>
                             </Nav>
                             <Form className="d-flex">
                                 <Form.Control
